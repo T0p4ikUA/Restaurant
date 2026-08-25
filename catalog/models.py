d6d471a6ef474dee4e08a02480fdb7ae78300517
Tymur <t.balenko12008@gmail.com>
2026-08-25 16:24:07 +0200

    feat: add Employee model inhering from AbstractUser

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Position(models.Model):
@@ -8,3 +9,23 @@ class Position(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Employee(AbstractUser):
+    username = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="employees",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self):
+        return (f"{self.username}, "
+                f"(is_active: {self.is_active},"
+                f"position: {self.position})")
