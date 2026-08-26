4765865450ddb323d85b7feb82abbeb44a57a4d0
Tymur <t.balenko12008@gmail.com>
2026-08-26 17:40:38 +0200

    feat:add dynamic counters to index view and template

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Position, Employee, Dish, Order
+
+
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "catalog/index.html")
+    num_pos = Position.objects.count()
+    num_emp = Employee.objects.count()
+    num_dis = Dish.objects.count()
+    num_ord = Order.objects.count()
+    context = {
+        "num_pos": num_pos,
+        "num_emp": num_emp,
+        "num_dis": num_dis,
+        "num_ord": num_ord,
+    }
+    return render(request, "catalog/index.html", context)
