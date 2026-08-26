fb0afa18cf95f2b9a460a331356f678470312097
Tymur <t.balenko12008@gmail.com>
2026-08-26 17:44:28 +0200

    feat: add visit counter to index view

--- catalog/views.py
+++ catalog/views.py
@@ -9,10 +9,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_emp = Employee.objects.count()
     num_dis = Dish.objects.count()
     num_ord = Order.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
     context = {
         "num_pos": num_pos,
         "num_emp": num_emp,
         "num_dis": num_dis,
         "num_ord": num_ord,
+        "num_visits": num_visits,
     }
     return render(request, "catalog/index.html", context)
